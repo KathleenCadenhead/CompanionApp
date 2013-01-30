61eb2b9fa6cf265c5365e05b4d630623489e704a
Kathryn Mitchell <Katie_Mitchell@baylor.edu>
2013-01-29 21:15:05 -0600

    Uploading Android tutorial

---
+++ MyFirstApp/com/example/lerpzapp/Achievements.java
@@ -0,0 +1,29 @@
+package com.example.lerpzapp;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class Achievements extends Activity {
+
+	@SuppressLint("NewApi")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+
+	    // Get the message from the intent
+	    Intent intent = getIntent();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_achievements, menu);
+		return true;
+	}
+
+}
---
+++ MyFirstApp/com/example/lerpzapp/LerpzProfile.java
@@ -0,0 +1,38 @@
+package com.example.lerpzapp;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class LerpzProfile extends Activity {
+
+	@SuppressLint("NewApi")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+
+	    // Get the message from the intent
+	    Intent intent = getIntent();
+
+	    // Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(40);
+	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
